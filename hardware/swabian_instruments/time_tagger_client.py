c6a76bf07c40b4841c409c9a0c914f552ce61a0f
Vladislav <bushmakinvls@gmail.com>
2022-03-07 11:21:52 +0100

    set up confocal + timetaggr coutnter

--- hardware/swabian_instruments/time_tagger_client.py
+++ hardware/swabian_instruments/time_tagger_client.py
@@ -3,16 +3,16 @@ from qtpy import QtCore
 
 from core.module import Base
 from core.configoption import ConfigOption
-from core.pi3_utils import delay
+#from core.pi3_utils import delay
 from logic.generic_logic import GenericLogic
 
 
 import socket
 import numpy as np
-try:
-    import cPickle as pickle
-except:
-    import pickle
+# try:
+#     import cPickle as pickle
+# except:
+import pickle
 import time
 
 def connect(func):
@@ -55,21 +55,22 @@ class TimeTaggerClient(Base):
         action = None if action == '' else action
         self.tcp_client.sendall(request.encode())
         try:
-            received = self.tcp_client.recv(10000) #TODO add length header and listen only to the specified bits!
+            received = self.tcp_client.recv(1024) #TODO add length header and listen only to the specified bits!
         except EOFError as e:
             raise e
         # response = pickle.loads(, encoding='bytes')
         # out_s = StringIO()
-        response = pickle.loads(received[1:])#, 'latin-1'))
- 
         flag = received[:1].decode()
+        response = pickle.loads(received[1:], encoding='latin1')
+ 
+        
         if flag == 'c':
             #get wavelength
             self.wlm_time = np.vstack((self.wlm_time, response))
             return response[0]
         elif flag == 'k':
             if action != None:
-                msg = pickle.dumps(action, protocol=pickle.HIGHEST_PROTOCOL)
+                msg = pickle.dumps(action, protocol=2)
                 self.tcp_client.sendall(msg)
             else:
                 print("Set action! ")
@@ -80,14 +81,34 @@ class TimeTaggerClient(Base):
         self.tcp_client.close()
     # setExposure(self, exposureTime):
         # return self.send_request("set_exposure_time", action=str(exposureTime* 1000))
-    def set_counter(self):
-        return self.send_request("set_counter", action=self._counter)
+    def set_counter(self, counter_params=None):
+        if counter_params is None:
+            counter_params = self._counter
+        return self.send_request("set_counter", action=counter_params)
 
     def get_counter(self):
         return self.send_request("get_counter")
         
     def get_server_time(self):
         return self.send_request("get_server_time")
+
+    def get_combined_channels(self):
+        return self.send_request("get_combined_channels")
+
+    def set_combined_channels(self, channels = [0,1]):
+        return self.send_request("set_combined_channels", action=channels)
+
+    def reset_time_tagger(self):
+        return self.send_request("reset_time_tagger")
+
+    def set_count_between_markers(self, params=None):
+        if params is None:
+            print("SET THE PARAMS")
+            return 
+        self.send_request("set_count_between_markers", action = params)
+
+    def get_count_between_markers(self):
+        return self.send_request("get_count_between_markers")
 
     def sync_clocks(self):
         # to sync time stamps and wavelengths add delta t to the current time of the client
